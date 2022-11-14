bcbbeed33725f191ad0fa7c9c377229b610748de
Cameron Hirsh <camjhirsh@gmail.com>
2022-11-13 23:26:34 -0800

    starting over using jquery

--- assets/index.js
+++ assets/index.js
@@ -1,23 +1,29 @@
-// Vanilla Javascript //
+// JQuery //
 
-const body = document.body;
+$( "body" ).append( '<div class="wrapper"> </div>' );
+$( ".wrapper" ).append( '<h1 class="title"> CAMERON HIRSH </h1>' );
 
-function addThing () {
-  const newDiv = document.createElement("div");
-  const newContent = document.createTextNode("Hi there and greetings!");
-  newDiv.appendChild(newContent);
-  const currentDiv = document.getElementsByClassName("wrapper")[0];
-  body.insertBefore(newDiv, currentDiv);
-}
+// setTimeOut(function() {
+//   addThing()
+// }, 3000)
 
-function stopSpinner () {
-  const spinnerDiv = document.getElementsByClassName("spinner")[0];
-  spinnerDiv.remove();
-}
 
-setTimeout(function() {
-  stopSpinner()
-}, 2999);
+// function stopSpinner () {
+//   const spinnerDiv = document.getElementsByClassName("spinner")[0];
+//   spinnerDiv.remove();
+// }
+
+// setTimeout(function() {
+//   stopSpinner()
+// }, 2999);
 
 // setTimeOut(addThing(), 3000);
+
+/*
+1. spinner fades out
+2. Top three kinda fade in
+3. One by one, the 4 links come in
+
+*/
+
 
