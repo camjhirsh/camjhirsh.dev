c5135b69006125c561614ca06801c7624810e8a6
Cameron Hirsh <camjhirsh@gmail.com>
2022-11-13 21:58:04 -0800

    fade spinner away

--- assets/index.js
+++ assets/index.js
@@ -6,24 +6,18 @@ function addThing () {
   const newDiv = document.createElement("div");
   const newContent = document.createTextNode("Hi there and greetings!");
   newDiv.appendChild(newContent);
-  const currentDiv = document.getElementsByClassName("wrapper")[0]
-  document.body.insertBefore(newDiv, currentDiv);
+  const currentDiv = document.getElementsByClassName("wrapper")[0];
+  body.insertBefore(newDiv, currentDiv);
+}
+
+function stopSpinner () {
+  const spinnerDiv = document.getElementsByClassName("spinner")[0];
+  spinnerDiv.remove();
 }
 
 setTimeout(function() {
-  addThing()
-}, 5000);
+  stopSpinner()
+}, 2999);
 
 // setTimeOut(addThing(), 3000);
 
-// function removeFadeOut( el, speed ) {
-//   var seconds = speed/1000;
-//   el.style.transition = "opacity "+seconds+"s ease";
-
-//   el.style.opacity = 0;
-//   setTimeout(function() {
-//       el.parentNode.removeChild(el);
-//   }, speed);
-// }
-
-// removeFadeOut(document.getElementById('test'), 2000);
